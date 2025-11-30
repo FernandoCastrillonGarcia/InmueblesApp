a7a2926b1ee2ddf59017b0c4c3e09e64d926dec7
FernandoCastrillonGarcia <fercastrigar500@proton.me>
2025-11-29 20:52:14 -0500

    modificar el nombre de la carpeta del proyecto

--- src/steps/data_loader.py
+++
@@ -1,9 +0,0 @@
-from zenml import step
-import pandas as pd
-
-@step
-def load_data() -> pd.DataFrame:
-    """Load raw property data"""
-    df = pd.read_csv('data/raw/data_v1.csv', sep=';')
-    print(f"Loaded {len(df)} properties")
-    return df
